ff9beb4f5470f65df1013c761e8649b596f7239e
Qui <lquiitfuture@gmail.com>
2019-12-19 22:18:46 +0700

    Update

--- Program.cs
+++ Program.cs
@@ -4,18 +4,33 @@ namespace Ref_Returns_and_Ref_Locals
 {
     class Program
     {
+        //Passing by Value in C#
+        /* static void Main(string[] args)
+         {
+             int no = 1;
+             PassByValue(no);
+             Console.WriteLine($"After the invocation of {nameof(PassByValue)} method, {nameof(no)} = {no}");
+             Console.WriteLine("Press any key to Exit.");
+             Console.ReadLine();
+         }
+         static void PassByValue(int x)
+         {
+             x = 2;
+
+         }*/
+
+        //Passing by Reference in C#
         static void Main(string[] args)
         {
-            int no = 2;
-            PassByValue(no);
-            Console.WriteLine($"After the invocation of {nameof(PassByValue)} method, {nameof(no)} = {no}");
+            int no = 1;
+            PassByReference(ref no);
+            Console.WriteLine($"After the invocation of {nameof(PassByReference)} method, {nameof(no)} = {no}");
             Console.WriteLine("Press any key to Exit.");
             Console.ReadLine();
         }
-        static void PassByValue(int x)
+        static void PassByReference(ref int x)
         {
-            int x1 = x;
-
+            x = 2;
         }
     }
 
