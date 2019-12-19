a481a100d61e03370710b5e2f4fccd52fa3e351e
Qui <lquiitfuture@gmail.com>
2019-12-19 21:48:09 +0700

    addproject

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ref_Returns_and_Ref_Locals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int no = 2;
+            PassByValue(no);
+            Console.WriteLine($"After the invocation of {nameof(PassByValue)} method, {nameof(no)} = {no}");
+            Console.WriteLine("Press any key to Exit.");
+            Console.ReadLine();
+        }
+        static void PassByValue(int x)
+        {
+            int x1 = x;
+
+        }
+    }
+
+}
